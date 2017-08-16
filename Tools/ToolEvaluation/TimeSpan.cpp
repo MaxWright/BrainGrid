5aef9a2d277e78beec871adc75b57c0df997defa
Moop\Kate <kate@katesprague.codes>
2017-08-15 22:06:05 -0700

    Testing to see if Jenkins server is operational!

---
+++ Tools/ToolEvaluation/TimeSpan.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //for testing
+using namespace std;
+//Code by Kate Sprague -- used for testing whether BrainGrid Jenkins is currently operational
+
+Test::Test() //default constructor sets everything to 0
+{
+	//nothing here except one int 
+	int test = 0;
+}
